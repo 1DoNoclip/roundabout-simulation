2faa72c1cc2a2321bbb1d7339334ff80b2b6f79e
1DoNoclip <samuelfrith08@gmail.com>
2026-07-08 16:08:09 +0100

    Replace number_of_arms with arms vector

--- src/blueprint/mod.rs
+++ src/blueprint/mod.rs
@@ -3,15 +3,15 @@ use crate::*;
 #[derive(Resource, Reflect)]
 /// Represents global intersection data.
 pub struct IntersectionBlueprint {
+    /// Length between 3 -> 6.
+    /// Max 2 entry lanes allowed when there are 3 arms.
+    pub arms: Vec<ArmBlueprint>,
     /// A number 1 -> 3.
     /// If number_of_entry_lanes == 3, then there is a dedicated left turn lane and
     /// exit lane separate to the roundabout.
     /// This dedicated lane does not need to yield to roundabout traffic,
     /// although will have to yield when merging back on the exit road.
     pub number_of_entry_lanes: usize,
-    /// A number 3 -> 6.
-    /// Max 2 entry lanes allowed when there are 3 arms.
-    pub number_of_arms: usize,
     /// Speed limit in ms-1
     pub speed_limit: f32,
     /// A greater deflection radius causes a smoother entry onto the roundabout.
